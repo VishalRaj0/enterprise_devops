64276cbae36cd3c565db3e6c5efdf15dfbeb7cd8
Vishal <thakurcmp1@gmail.com>
2025-05-25 03:54:39 +0530

    first-commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace enterprise_devops.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
